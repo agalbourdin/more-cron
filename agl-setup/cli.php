1c20ce2d005bf644153296f8e1cddd120c7ebde3
Alexis Galbourdin <i@alexisg.net>
2013-12-26 15:18:49 +0100

    Updated setup config.

--- agl-setup/cli.php
+++ agl-setup/cli.php
@@ -7,17 +7,17 @@ return array(
     'file:copy' => array(
         array(
             $currentDir . 'app/etc/config/more/cron/main.php',
-            APP_PATH    . 'app/etc/config/more/cron/main.php'
+            $appPath    . 'app/etc/config/more/cron/main.php'
         ),
 
         array(
             $currentDir . 'app/etc/config/more/cron/samples/main.sample.php',
-            APP_PATH    . 'app/etc/config/more/cron/samples/main.sample.php'
+            $appPath    . 'app/etc/config/more/cron/samples/main.sample.php'
         ),
 
         array(
             $currentDir . 'cron.php',
-            APP_PATH    . 'cron.php'
+            $appPath    . 'cron.php'
         ),
     )
 );
